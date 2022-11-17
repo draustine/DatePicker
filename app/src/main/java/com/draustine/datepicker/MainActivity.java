9ad4df07b32a138d0a9f9665e3ba40bb74d4b125
Austine Ezemelue <draustine@yahoo.com>
2022-11-17 15:57:49 +0100

    Finished

--- app/src/main/java/com/draustine/datepicker/MainActivity.java
+++ app/src/main/java/com/draustine/datepicker/MainActivity.java
@@ -3,12 +3,60 @@ package com.draustine.datepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialTextView tvDate;
+    MaterialButton pickDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDate = findViewById(R.id.textView);
+        pickDate = findViewById(R.id.dateButton);
+
+
+
+
+        pickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+
+    }
+
+    private void showDatePicker() {
+
+        MaterialDatePicker datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Date").build();
+
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+
+                tvDate.setText(datePicker.getHeaderText());
+            }
+        });
+
+
+
+
+
+
+        datePicker.show(getSupportFragmentManager(), "TAG");
+
+
+
     }
 }
